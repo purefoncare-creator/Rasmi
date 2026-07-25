e93a41d3b154b71a5ddab3d6aad81ee37aeb3980
abohmam <abohmam@kali>
2026-07-25 18:37:20 -0400

    chore: bump version to v1.1.8 (versionCode=18)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.rasmi.purevon"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.1.7"
+        versionCode = 18
+        versionName = "1.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
